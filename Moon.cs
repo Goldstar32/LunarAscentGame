8fd1b2022485370816978f5b4f3d9b12297a8cdc
Goldstar32 <93144472+Goldstar32@users.noreply.github.com>
2025-01-24 08:11:28 +0100

    Moon update
    Added texture to moon and made it more dynamic when changing size

--- Moon.cs
+++ Moon.cs
@@ -5,11 +5,16 @@ public partial class Moon : StaticBody3D
 {
     // Constants vvv
 
-    const double moonRadius = 1737.4e3; // Constant for moons radius
+    const float moonRadius = 1000f; // Constant for moons radius 1737.4e3f
+    const double moonMass = 1e15f; // Constant for moons mass 7.342e22
 
     // Constants ^^^
     //
     // Properties vvv
+
+    private CollisionShape3D CollisionShape { get; set; } // Property for collision shape
+
+    private MeshInstance3D MeshInstance { get; set; } // Property for mesh
 
     public double MoonMass { get; set; } // Property for moons mass
 
@@ -21,16 +26,24 @@ public partial class Moon : StaticBody3D
     //
     // Methods vvv
 
-    // Constructor
-    public Moon() { }
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        // Initialize properties by getting the nodes
+        CollisionShape = GetNode<CollisionShape3D>("MoonCollisionShape");
+        MeshInstance = GetNode<MeshInstance3D>("MoonMesh");
+
         // Set initial values
-        MoonMass = 7.342e22; // Mass of moon
-        Radius = moonRadius; // Radius of moon
-        this.GlobalPosition = new Vector3(0, -(float)moonRadius, 0); // Center of moon (origo is approx at surface of moon)
+        MoonMass = moonMass; // Set mass of moon
+        Radius = moonRadius; // Set radius of moon
+        if (CollisionShape.Shape is SphereShape3D sphereShape) // Make sure shape is sphere
+            sphereShape.Radius = moonRadius; // Set collision shape's radius
+        if (MeshInstance.Mesh is SphereMesh sphereMesh) // Make sure shape is sphere
+        {
+            sphereMesh.Radius = moonRadius;
+            sphereMesh.Height = moonRadius * 2;
+        }
+        this.GlobalPosition = new Vector3(0, -moonRadius, 0); // Center of moon (origo is approx at surface of moon)
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
